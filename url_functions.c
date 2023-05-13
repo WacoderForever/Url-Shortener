c4b905834cf2d077667ea668fdd515840701a2aa
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-13 19:17:51 -0300

    att

--- url_functions.c
+++ url_functions.c
@@ -13,6 +13,8 @@ char *gemerate_random_token(int size) {
 
     srand(time(NULL));
 
+
+
     for (int i = 0; i < size; ++i) {
         int index = rand() % total_size;
         token[i] = chars[index];
@@ -32,6 +34,16 @@ char * return_content_of_token(char *token){
 char *create_randon_url_token(char *url){
     char *token;
     //ensure its not equal to other token
+
+    char *formated_url[2000];
+
+    if(dtw_starts_with(url,"http://") || dtw_starts_with(url,"https://")){
+        sprintf(formated_url,"%s",url);
+    }else{
+        sprintf(formated_url,"http://%s",url);
+    }
+
+
     while(true){
         token = gemerate_random_token(6);
 
@@ -40,7 +52,7 @@ char *create_randon_url_token(char *url){
         if(content == NULL){
             char formated_path[30];
             sprintf(formated_path,"data/tokens/%s",token);
-            dtw_write_string_file_content(formated_path,url);
+            dtw_write_string_file_content(formated_path,formated_url);
             return token;
         }
         free(token);
