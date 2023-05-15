7491fc634c08c94d7694a6c1fa59910c99418ccc
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-15 09:12:23 -0300

    att

--- url_functions.c
+++ url_functions.c
@@ -44,15 +44,35 @@ char *create_randon_url_token(char *url){
     }
 
 
+
     while(true){
         token = gemerate_random_token(6);
 
         char *content = return_content_of_token(token);
 
         if(content == NULL){
-            char formated_path[30];
-            sprintf(formated_path,"data/tokens/%s",token);
-            dtw_write_string_file_content(formated_path,formated_url);
+            char formated_url_path[30];
+            sprintf(formated_url_path,"data/tokens/%s",token);
+            dtw_write_string_file_content(formated_url_path,formated_url);
+
+
+            char formated_json_path[30];
+            sprintf(formated_json_path,"data/statistc/%s.json",token);
+            cJSON *json = cJSON_CreateObject();
+            //get unix time from now 
+            time_t now = time(NULL);
+            cJSON_AddNumberToObject(json,"created_at",now);
+            cJSON_AddNumberToObject(json,"clicks",0);
+            
+            char *json_string = cJSON_Print(json);
+            dtw_write_string_file_content(formated_json_path,json_string);
+            cJSON_Delete(json);
+            free(json_string);
+
+
+
+
+
             return token;
         }
         free(token);
